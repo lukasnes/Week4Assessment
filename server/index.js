4461bd7357d7d468e7977df45b3f0de23165805f
Lukas Nesittka <97050688+LukasNesittka@users.noreply.github.com>
2022-01-30 16:33:15 -0600

    Final

--- server/index.js
+++ server/index.js
@@ -1,12 +1,6 @@
 const express = require("express");
 const cors = require("cors");
 const app = express();
-const names = []
-const {
-  changeName,
-  deleteName,
-  doThis
-} = require("./ctrl.js")
 
 app.use(cors());
 app.use(express.json()); // When we want to be able to accept JSON.
@@ -45,8 +39,61 @@ app.get("/api/fortune", (req,res) => {
   res.status(200).send(randomFortune)
 })
 
-app.post("/api/toDoList", doThis)
-app.put("/api/updateName", changeName)
-app.delete("/api/deleteName/:name", deleteName)
+
+const names = []
+const theLost = []
+let id = 1
+
+app.post("/api/toDoList", (req,res) => {
+  let { name } = req.body
+  
+  let newName = {
+    id,
+    name: name
+  }
+  for(i = 0; i < names.length; i++) {
+    if(names[i].name === newName.name){
+      res.status(406).send("That name already exists")
+      return
+    }
+  }
+  names.push(newName)     
+  id++
+  res.status(200).send(names)
+
+})
+app.put("/api/updateName", (req,res) => {
+  const {check,update} = req.body
+  console.log(check)
+  console.log(names)
+  for(i = 0; i < names.length; i++) {
+    if(check === names[i].name){
+      for(j = 0; j < names.length; j++) {
+        if(update === names[j].name){
+          res.status(400).send("That name already exists")
+          return
+        }
+      }
+      names[i].name = update
+      res.status(200).send(names)
+    }
+  }
+})
+app.delete("/api/deleteName/:id", (req,res) => {
+  const nameId = +req.params.id
+  console.log(nameId)
+
+  const tgtInd = names.findIndex(nameObj => {
+    return nameObj.id === nameId
+  })
+  console.log(tgtInd)
+  id--
+  const removed = names.splice(tgtInd, 1)
+  theLost.push(removed[0])
+  res.status(200).send([removed[0], names])
+})
+app.get("/api/theLost", (req,res) => {
+  res.status(200).send(theLost)
+})
 
 app.listen(4000, () => console.log("Server running on 4000"));
